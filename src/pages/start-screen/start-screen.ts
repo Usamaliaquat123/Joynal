a39cc5f969ee2d80c5abccf27b7621af932f376b
Usamaliaquat123 <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2018-08-06 20:36:33 +0500

    implementing twitter and facebook libr

--- src/pages/start-screen/start-screen.ts
+++ src/pages/start-screen/start-screen.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
-
+import { TwitterConnect } from "@ionic-native/twitter-connect";
 @IonicPage()
 @Component({
   selector: 'page-start-screen',
@@ -9,12 +9,22 @@ import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 })
 export class StartScreenPage {
 
-  constructor(public fb : Facebook,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public twitter :  TwitterConnect,public fb : Facebook,public navCtrl: NavController, public navParams: NavParams) {
   }
-
+  // Bug : todo : fixing tomorrow at developer.facebook.com implementing 28 char hash
+  // Facebook Authentication
   fbAuth(){
     this.fb.login(['public_profile', 'user_friends','email']).then((res : FacebookLoginResponse) => console.log('users data' , res));
   }
+  // todo : Requesting for twiter developer account the application is under reviewing
+  // Twitter Authentication
+  twitterAuth(){
+    this.twitter.login();
+  }
+  // todo : Implementing instagram authentication tomorrow
+  // Instagram Authentication
+  InstaAuth(){
 
+  }
 
 }
