3aeed0f88982bfb1bb41062af279105482277ce8
Usamaliaquat123 <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2018-09-05 10:00:10 +0500

    'implementWelcomeLogin'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,26 @@ import {LoginPage} from "../pages/Authentications/login/login";
 import firebase from 'firebase'
 import { firebaseKeys } from "../config/keys";
 import { LocalNotifications } from "@ionic-native/local-notifications";
-
+import { Storage } from "@ionic/storage";
 @Component({
   templateUrl: 'app.html'
 })  
 export class MyApp {
-  rootPage:string = "LoginPage";
-
-  constructor(alertCtrl: AlertController,localNotify : LocalNotifications,platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage:string;
+  welcomeScreen : boolean;
+  constructor(storage : Storage,alertCtrl: AlertController,localNotify : LocalNotifications,platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
     
+        storage.get('session.welcomePage').then(res => {
+          if(res){
+            this.rootPage = 'LoginPage'
+          }else{
+            storage.set('session.welcomePage',this.welcomeScreen = true);
+            this.rootPage = 'WelcomeScreenPage';
+          }
+        });
       statusBar.styleDefault();
       splashScreen.hide();
     });
